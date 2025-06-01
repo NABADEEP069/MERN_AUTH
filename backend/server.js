4965ac248cd8593c65227a756d92574e07f7bcda
NABADEEP KR. DAS <nabadeepdas069@gmail.com>
2025-06-01 20:33:16 +0530

    shitty UI

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,20 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('server is ready');
 })
+
+
+app.get('/name', (req, res) => {
+    const name = [
+        { name: 'Nobo', age: 30 },
+        { name: 'Prashant', age: 25 },
+        { name: 'X', age: 28 }
+    ];
+    res.json(name);
+});  
+
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
+
